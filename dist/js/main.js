7fb04cf149eddb2932f0a5c21011171294a765f1
irahulsaini <irahulsaini01@gmail.com>
2022-05-05 22:36:56 +0530

    Update main.js

--- dist/js/main.js
+++ dist/js/main.js
@@ -171,7 +171,7 @@ function check_pwd(){
 }
 $('#password_confirm').submit(function(e){
 	e.preventDefault();
-	if($('#get_pwd').val() == 'fuck'){
+	if($('#get_pwd').val() == 'fuck@boobs'){
 		setCookie('password',1,24);
 		pwd_modal.hide();
 
