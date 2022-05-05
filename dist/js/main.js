8fdad517addf52e4c22ed929e0d14343d3087f7d
irahulsaini <irahulsaini01@gmail.com>
2022-05-05 12:09:58 +0530

    Upload

---
+++ dist/js/main.js
@@ -0,0 +1,105 @@
+$('.current_url').val(window.location.href);
+$('.contact-form').submit(function(e){
+	e.preventDefault();
+	var form = $(this);
+	var $btn = $('.btn-submit',form);
+	var btn_val = $btn.html();
+	var $response = $('.response',form);
+	$response.html('<i class="fa fa-sync fa-spin"></i>');
+	$btn.attr('disabled',1);
+	$.ajax({
+		url:base_url+'/api/contact',
+		data:$(this).serializeArray(),
+		type:'post',
+		success:function(r){
+			if(r == 1){
+				$response.html('<div class="alert alert-success mt-2">Thank You! Your Message has been Sent.</div>')
+				window.location = base_url + '/thank-you/';
+			}else{
+				if(r){
+					$response.html('<div class="alert alert-danger mt-2"><strong>Error: </strong>'+r+'</div>')
+				}else{
+					$response.html('<div class="alert alert-danger mt-2">Unable to Send Message, Please try again later</div>')
+				}
+				
+				$btn.removeAttr('disabled');
+			}
+		},
+		error:function(){
+			$response.html('<div class="alert alert-danger mt-2">Unable to Connect Server, Please try again later</div>')
+			$btn.removeAttr('disabled');
+		}
+	})
+});
+function getGet(p){
+	var url = new URL(window.location.href);
+	var q = url.searchParams.get(p);
+	return q==null?'':q;
+}
+function get_posts(output){
+	if(page == null || !page){
+		page = 1;
+	}
+
+	var api_url = api_base;
+	if(query){
+		api_url += 'search/'+'?_embed&per_page='+per_page+'&category='+category+'&page='+page+'&search='+query;
+	}else{
+		api_url += 'posts/'+'?_embed&per_page='+per_page+'&category='+category+'&page='+page;
+	}
+	$.get(api_url,function(d){
+		
+		if(d.length > 0){
+			var rows = d;
+			var html = '<div class="row">';
+			$.each(rows,function(k,v){
+				if(query){
+					html += output_post(v._embedded.self[0]);	
+				}else{
+					html += output_post(v);
+				}
+				
+			});
+			html += '</div>';
+			$(output).html(html);
+		}
+	});
+
+}
+
+function output_thumbs(t){
+	if(!t || t==''){
+		return '';
+	}
+	var t = t.split(';');
+	var th = [];
+	for(i=0;i<t.length;i++){
+		if(i==0){
+			th.push('<img class="featured" src="'+t[i]+'"/>');
+		}else{
+			th.push('<img src="'+t[i]+'"/>');	
+		}
+	}
+	return th.join('');
+}
+
+function sec2hour(s){
+	return new Date(s * 1000).toISOString().substr(11, 8);
+
+}	
+
+
+function output_post(v){
+	var thumb='',duration=0;
+	if(v.metadata && v.metadata.thumbs){
+		thumb = v.metadata.thumbs[0];
+	}
+	if(v.metadata && v.metadata.duration){
+		duration = v.metadata.duration[0];
+	}
+	var html = '<div class="col-6 col-sm-6 col-md-4 col-lg-2 pe-2 pb-4"><a href="#" class="video-list-item" title="'+v.title.rendered+'">';
+	html += '<div class="image">'+output_thumbs(thumb)+'<div class="video-meta"><span class="duration"><i class="fa fa-clock me-1"></i>'+sec2hour(duration)+'</span></div></div>';
+	html += '<div href="#" class="title" >'+v.title.rendered+'</div>';
+	html += '</a></div>';
+	return html;
+}	
