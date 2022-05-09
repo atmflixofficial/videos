daf5e8943c0e751f0f245f5461ebf16bc8c578d9
irahulsaini <irahulsaini01@gmail.com>
2022-05-09 22:29:03 +0530

    Update main.js

--- dist/js/main.js
+++ dist/js/main.js
@@ -198,7 +198,7 @@ function get_post(id,output){
 		$(output).html('');
 		if(cat.length > 0){
 			get_posts('#related_posts',{
-				'category':cat.join(','),
+				'category':cat[Math.floor(Math.random()*cat.length)];,
 				'per_page':12,
 				'cols':4,
 				'order':'DESC',
